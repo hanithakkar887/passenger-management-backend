a55b913f4859139e6d1fbec98f1aaf954d709e84
hanithakkar887 <hanithakkar887@gmail.com>
2025-03-03 19:41:02 +0530

    Update3

--- models/Passenger.js
+++ models/Passenger.js
@@ -1,31 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const passengerSchema = new mongoose.Schema({
-//     name: { type: String, required: true },
-//     age: { type: Number, required: true },
-//     gender: { type: String, required: true, enum: ["Male", "Female", "Other"] },
-//     contact: { type: String },
-//     email: { type: String },
-//     photo: { type: String }, // File path for photo
-//     idCard: { type: String }, // File path for ID card
-// });
-
-// module.exports = mongoose.model("Passenger", passengerSchema);
-
-
-// const mongoose = require("mongoose");
-
-// const passengerSchema = new mongoose.Schema({
-//     name: String,
-//     age: Number,
-//     gender: String,
-//     contact: String,
-//     email: String,
-//     photo: String,   // Store file path of image
-//     idCard: String,  // Store file path of PDF
-// });
-
-// module.exports = mongoose.model("Passenger", passengerSchema);
 const mongoose = require("mongoose");
 
 const passengerSchema = new mongoose.Schema({
